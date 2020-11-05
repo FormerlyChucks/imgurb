b65e2ff1815dfa6e7c8b5de3fe94b366b294a90f
dio <61555147+Mr-Steal-Your-Script@users.noreply.github.com>
2020-11-05 15:05:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ reddit = praw.Reddit(client_id=config.c_id,client_secret=config.c_s,user_agent=c
 while True:
     try:
         subreddit = reddit.subreddit(random.choice(config.subs))
-        print('Random subreddit is:',subreddit)
         submission = random.choice(list(subreddit.top('all', limit=None)))
         if submission.domain in ['i.redd.it', 'i.imgur.com']:
-            print('Domain is in our list!')
             file_name = submission.url.replace('https://i.imgur.com/','').replace('https://i.redd.it/','')
             response = requests.get(submission.url)
             file = open(file_name, "wb")
@@ -28,14 +26,8 @@ while True:
                     f.write(f'{access_token} {refresh_token}')
             uploaded_image = im.upload_image(file_name, title=submission.title)
             uploaded_image.submit_to_gallery(title=submission.title)
-            print('Submitted!')
             os.remove(file_name)
-            print('Deleted File')
             time.sleep(300)
-                        
-        elif submission.domain not in ['i.redd.it', 'i.imgur.com']:
-            print('domain is not in domains :(')
-            time.sleep(10)
-            
+        elif submission.domain not in ['i.redd.it', 'i.imgur.com']: time.sleep(10)
     except Exception: time.sleep(60)     
     except KeyboardInterrupt: quit()
