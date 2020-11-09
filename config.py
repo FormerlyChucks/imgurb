babf90d5db8a525cc01ca818ac9a20efdeed31f0
dio <61555147+Mr-Steal-Your-Script@users.noreply.github.com>
2020-11-09 13:10:47 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 #Reddit Configuration
-c_id=''#ID
-c_s=''#Secret
-u_a=''#User Agent
-un=''#Username
-pw=''#Password
+c_id = ''#ID
+c_s = ''#Secret
+u_a = ''#User Agent
+un = ''#Username
+pw = ''#Password
 subs = []#Subreddits
 #Imgur Configuration
 i_id = ''#Imgur Client ID
