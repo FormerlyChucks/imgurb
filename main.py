adf5e0e83fd5f15f412f7ccb9f5b4f521f630c45
dio <61555147+Mr-Steal-Your-Script@users.noreply.github.com>
2020-11-13 14:29:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -32,6 +32,8 @@ while True:
                     f.write(f'{access_token} {refresh_token}')
             im.upload_image(file_name).submit_to_gallery(title=no_emoji)
             print('Uploaded To Gallery')
+            with open("ids.txt", "r") as file:
+              f.write(submission.id)
             os.remove(file_name)
             print('Deleted File')
             time.sleep(300)
