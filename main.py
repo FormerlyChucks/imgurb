4348b25f9e65c02d24adfd68e98c77c7ea4d4972
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-06-21 14:18:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import praw, random, webbrowser, pyimgur, os, requests
+import praw, random, webbrowser, pyimgur, os, requests, yaml
 
 client_id = ''
 client_secret = ''
@@ -7,20 +7,23 @@ imgur_id = ''
 subs = []
 domains = ['i.redd.it','i.imgur.com']
 
+with open("config.yaml") as cf:
+    config = yaml.safe_load(cf)
+
 try:
     with open('tokens.txt') as t:
         access_token, refresh_token = t.read().strip().split()
     im = pyimgur.Imgur(imgur_id, access_token=access_token, refresh_token=refresh_token)
 except FileNotFoundError:
-    im = pyimgur.Imgur(imgur_id)
+    im = pyimgur.Imgur(cf["imgur_id"])
     webbrowser.open(im.authorization_url('pin'))
     pin = input('Enter Your Pin: ')
     access_token, refresh_token = im.exchange_pin(pin)
     with open('tokens.txt', 'w') as t:
         t.write(f'{access_token} {refresh_token}')
 
-reddit = praw.Reddit(client_id=client_id,client_secret=client_secret,user_agent=user_agent)
-subreddit = reddit.subreddit(random.choice(subs))
+reddit = praw.Reddit(client_id=cf["client_id"],client_secret=cf["client_secret"],user_agent=cf["user_agent"])
+subreddit = reddit.subreddit(random.choice(cf["subs"]))
 submissions = list(subreddit.top('all', limit=1000))
 submissions = [submission for submission in submissions if submission.domain in domains and '.gifv' not in submission.url and submission.over_18==False and len(submission.title)<=225]
 submission = random.choice(submissions)
